81b9baa51fc23bc85ff360e4074f1c415c852de9
SashaSub <ssuboch@gMAIL.com>
2023-06-15 11:40:19 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :comments, dependent: :destroy
+  has_many :posts       
+  has_many :images, as: :imageable
+  #has_one_attached :image #, as: :imageable
+  accepts_nested_attributes_for :images
+
+  validates :images, presence: true
+  enum :role, [:normal_user, :admin]
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :posts
+  resources :comments
+  devise_for :users, controllers: { 
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations' 
+  }
+  
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "posts#index"
+end
