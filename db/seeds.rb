adaf43914b9a7778377a7d4f3b8aac8a1810e9cf
SashaSub <ssuboch@gMAIL.com>
2023-06-25 16:31:06 -0400

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name:"Admin Karina", email:"kari@correo.cl", password:"karipassword", role:"admin")
+User.create(name:"User Paul", email:"user@correo.cl", password:"useruser")
