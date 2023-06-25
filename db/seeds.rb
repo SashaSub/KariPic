d19c25bad9076e0f0c1f0bf709329a69a19f2760
SashaSub <ssuboch@gMAIL.com>
2023-06-25 19:27:58 -0400

    add admin role

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name:"Admin Karina", email:"kari@correo.cl", password:"karipassword", role:"admin")
-User.create(name:"User Paul", email:"user@correo.cl", password:"useruser")
+#User.create(name:"Admin Karina", email:"kari@correo.cl", password:"karipassword", role:"admin", image: "https://www.pngkey.com/png/full/203-2037403_flat-faces-icons-circle-girl-flat-icon-png.png")
+#User.create(name:"User Paul", email:"user@correo.cl", password:"useruser", image: "https://media.istockphoto.com/id/1060493434/vector/female-call-center-avatar.jpg?s=170667a&w=0&k=20&c=TZHmyVDPL3nR_Lr0kdihblq0Oj-WL38r-ERWcvsBd0M=")
+
+User.update(role:"admin") where user.email = "kari@gmail.com"
