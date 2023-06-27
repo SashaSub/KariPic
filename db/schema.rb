0ec999396763e17ef3a4871dbe2e99b99b75cb26
SashaSub <ssuboch@gMAIL.com>
2023-06-27 17:31:14 -0400

    changing roles 2

--- db/schema.rb
+++ db/schema.rb
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_14_231914) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role", default: 1
+    t.integer "role", default: "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
